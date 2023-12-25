2eb906da6f4a2de0f04b77da375d839ae1f0b0dd
PazitoPazos <javier.pazos001@gmail.com>
2023-12-25 13:57:36 +0100

    Properties

---
+++ Clases selladas/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace Clases_selladas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lagartija Juancho = new("Juancho");
+            Juancho.GetNombre();
+
+            Humano Juan = new("Juan");
+            Juan.GetNombre();
+        }
+    }
+
+    interface IMamiferosTerrestres
+    {
+        int NumeroPatas();
+    }
+    interface IAnimalesYDeportes
+    {
+        string TipoDeporte();
+        bool EsOlimpico();
+    }
+    interface ISaltoConPatas
+    {
+        int NumeroPatas();
+    }
+
+    abstract class Animal
+    {
+        public void Respirar() => Console.WriteLine("Soy capaz de respirar");
+
+        public abstract void GetNombre();
+    }
+
+    class Mamifero(string nombre) : Animal
+    {
+        public string? NombreSerVivo = nombre;
+
+        public virtual void Pensar() => Console.WriteLine("Pensamiento instintivo básico");
+
+        public void CuidarCrias() => Console.WriteLine("Cuido de mis crías");
+
+        public override void GetNombre()
+        {
+            Console.WriteLine($"El nombre del mamífero es: {NombreSerVivo}");
+        }
+    }
+
+    class Lagartija(string nombre) : Animal
+    {
+        public string NombreReptil = nombre;
+
+        public override void GetNombre()
+        {
+            Console.WriteLine($"El nombre del reptil es: {NombreReptil}");
+        }
+    }
+
+    class Ballena(string nombre) : Mamifero(nombre)
+    {
+        public void Nadar() => Console.WriteLine("Soy capaz de nadar");
+    }
+    class Caballo(string nombre) : Mamifero(nombre), IMamiferosTerrestres, IAnimalesYDeportes, ISaltoConPatas
+    {
+        int IMamiferosTerrestres.NumeroPatas()
+        {
+            return 4;
+        }
+
+        int ISaltoConPatas.NumeroPatas()
+        {
+            return 2;
+        }
+
+        public string TipoDeporte()
+        {
+            return "Hípica";
+        }
+
+        public bool EsOlimpico()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Galopar() => Console.WriteLine("Soy capaz de galopar");
+    }
+    class Humano(string nombre) : Mamifero(nombre)
+    {
+        public sealed override void Pensar() => Console.WriteLine("Soy capaz de pensar");
+    }
+
+    class Adolescente(string nombre) : Humano(nombre)
+    {
+        public override void Pensar()
+        {
+            Console.WriteLine("Las hormonas me impiden pensar");
+        }
+    }
+    sealed class Gorila(string nombre) : Mamifero(nombre), IMamiferosTerrestres
+    {
+        public int NumeroPatas()
+        {
+            return 2;
+        }
+
+        public override void Pensar() => Console.WriteLine("Pensamiento instintivo avanzado");
+
+        public void Trepar() => Console.WriteLine("Soy capaz de trepar");
+    }
+
+    class Chimpance(string nombre) : Gorila(nombre)
+    {
+
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado Juan = new Empleado("Juan");
+            Juan.SALARIO = -1200;
+            Juan.SALARIO += 500;
+            Console.WriteLine("El salario del empleado es: " + Juan.SALARIO);
+        }
+    }
+
+    class Empleado
+    {
+        private string _nombre;
+        private double _salario;
+
+        /*public double SALARIO
+        {
+            get { return _salario; }
+            set { _salario = evaluaSalario(value); }
+        }*/
+
+        public double SALARIO { get => _salario; set => _salario = evaluaSalario(value); }
+
+        public Empleado(string nombre) { 
+            _nombre = nombre;
+        }
+
+        public void setSalario(double salario)
+        {
+            if (salario < 0) {
+                Console.WriteLine("El salario no puede ser negativo. Se asignará 0 como salario");
+            } else
+            {
+                _salario = salario;
+            }
+        }
+
+        public double getSalario()
+        {
+            return _salario;
+        }
+
+        private double evaluaSalario(double salario)
+        {
+            if (salario < 0) return 0;
+            return salario;
+        }
+    }
+}
