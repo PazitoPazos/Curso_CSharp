ac040bc07ffd404cffd226b652e6e64cd949562f
PazitoPazos <javier.pazos001@gmail.com>
2023-12-27 16:45:34 +0100

    WPF - ListBox

--- WPF App/MainWindow.xaml.cs
+++ WPF App/MainWindow.xaml.cs
@@ -20,8 +20,50 @@ namespace WPF_App
         {
             InitializeComponent();
 
-            JuntaNombre JuntaNombre = new() { Nombre = "Juan", Apellido = "Díaz" };
-            this.DataContext = JuntaNombre;
+            List<Poblaciones> ListaPob = [];
+            ListaPob.Add(new Poblaciones() { Poblacion1 = "Madrid", Poblacion2 = "Barcelona", Temperatura1 = 15, Temperatura2 = 17, DiferenciaTemp = 2 });
+            ListaPob.Add(new Poblaciones() { Poblacion1 = "Valencia", Poblacion2 = "Alicante", Temperatura1 = 19, Temperatura2 = 20, DiferenciaTemp = 1 });
+            ListaPob.Add(new Poblaciones() { Poblacion1 = "Málaga", Poblacion2 = "Bilbao", Temperatura1 = 20, Temperatura2 = 7, DiferenciaTemp = 13 });
+            ListaPob.Add(new Poblaciones() { Poblacion1 = "Sevilla", Poblacion2 = "Coruña", Temperatura1 = 22, Temperatura2 = 8, DiferenciaTemp = 14 });
+            ListaPoblaciones.ItemsSource = ListaPob;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListaPoblaciones.SelectedItem != null)
+            {
+                MessageBox.Show((ListaPoblaciones.SelectedItem as Poblaciones)?.Poblacion1 + " " +
+                (ListaPoblaciones.SelectedItem as Poblaciones)?.Temperatura1 + "ºC " +
+                (ListaPoblaciones.SelectedItem as Poblaciones)?.Poblacion2 + " " +
+                (ListaPoblaciones.SelectedItem as Poblaciones)?.Temperatura2 + "ºC");
+            } else
+            {
+                MessageBox.Show("Selecciona un elemento");
+            }
+        }
+
+        private void TextBlock_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (ListaPoblaciones.SelectedItem != null)
+            {
+                MessageBox.Show((ListaPoblaciones.SelectedItem as Poblaciones)?.Poblacion1 + " " +
+                (ListaPoblaciones.SelectedItem as Poblaciones)?.Temperatura1 + "ºC " +
+                (ListaPoblaciones.SelectedItem as Poblaciones)?.Poblacion2 + " " +
+                (ListaPoblaciones.SelectedItem as Poblaciones)?.Temperatura2 + "ºC");
+            } else
+            {
+                MessageBox.Show("Selecciona un elemento");
+            }
+        }
+    }
+
+    public class Poblaciones
+    {
+        public string? Poblacion1 { get; set; }
+        public int Temperatura1 { get; set; }
+        public string? Poblacion2 { get; set; }
+        public int Temperatura2 { get; set; }
+
+        public int DiferenciaTemp {  get; set; }
     }
 }
