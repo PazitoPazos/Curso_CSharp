cc418537bd7b34e2d40e0fa5feede93ef1d2dbaa
PazitoPazos <javier.pazos001@gmail.com>
2023-12-28 14:37:01 +0100

    BBDD - Relaciones

---
+++ WPF Gestion Pedidos/GestionPedidosDataSet.cs
@@ -0,0 +1,14 @@
+﻿namespace WPF_Gestion_Pedidos
+{
+
+
+    public partial class GestionPedidosDataSet
+    {
+    }
+}
+namespace WPF_Gestion_Pedidos {
+    
+    
+    public partial class GestionPedidosDataSet {
+    }
+}
